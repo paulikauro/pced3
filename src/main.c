83baf8cb985e06428570a70b3142bd4bd16079df
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2019-11-22 21:26:57 +0200

    Add initial command line handling

--- src/main.c
+++ src/main.c
@@ -5,12 +5,15 @@
 #include "ui.h"
 #include "editor.h"
 
+char *parse_command_line(int argc, char *argv[]);
+
 /* Temporary kludge */
-int main() {
+int main(int argc, char *argv[]) {
+    char *filename = parse_command_line(argc, argv);
     Editor editor = {0};
     Buffer buf = {0};
     editor.current_buffer = &buf;
-    FILE *file = fopen("testfile", "r");
+    FILE *file = fopen(filename, "r");
     if (file == NULL) {
         error_std("fopen");
     }
@@ -25,4 +28,13 @@ int main() {
 
     buffer_free(editor.current_buffer);
     return 0;
+}
+
+char *parse_command_line(int argc, char *argv[]) {
+    /* TODO */
+    if (argc < 2) {
+        printf("usage: %s filename\n", argv[0]);
+        exit(1);
+    }
+    return argv[1];
 }

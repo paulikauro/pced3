7c67140f48642fa3fbf05ccea1ed741d904744a3
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2019-11-22 21:28:04 +0200

    Move editor_mode_to_string to editor.c

--- src/editor.c
+++ src/editor.c
@@ -1,9 +1,6 @@
 #include "editor.h"
 
-static const char *editor_mode_strings[] = {
+char *editor_mode_strings[] = {
     [EM_NORMAL] = "NORMAL",
 };
 
-const char *editor_mode_to_string(EditorMode mode) {
-    return editor_mode_strings[mode];
-}
--- src/editor.h
+++ src/editor.h
@@ -6,7 +6,10 @@ typedef enum EditorMode {
     EM_NORMAL,
 } EditorMode;
 
-const char *editor_mode_to_string(EditorMode mode);
+static inline char *editor_mode_to_string(EditorMode mode) {
+    extern char *editor_mode_strings[];
+    return editor_mode_strings[mode];
+}
 
 typedef struct Editor {
     EditorMode current_mode;
